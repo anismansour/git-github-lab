d3161b6f6cb8a8f4b1c2bb2aeae8b92b68ec7c79
Anis Mansour <anismansour@Aniss-MacBook-Pro.local>
2019-03-19 19:23:34 -0700

    Commit 9 - Created loops.js file.

---
+++ loop-questions/js/app.js
@@ -0,0 +1,58 @@
+let num = 0;
+while(num <= 99){
+console.log("the nb is  " +num);
+num = num +1; 
+};
+//exercice 2
+
+let num2 =99;
+while (num2>=0){
+    console.log("example 2    " + num2);
+    num2= num2 -1;
+};
+//exercice 3
+
+let num3 = 298;
+while(num3 >= 1)
+{
+console.log("counting dow by 3   " + num3);
+num3 = num3 - 3;
+};
+//exercice 4
+
+let num4 =0;
+while(num4 <=98) {
+console.log("counting up by 2   " + num4);
+num4 =num4 + 2;
+};
+
+//exercice 6
+let num6 = 49;
+while (num6 <= 72) {
+console.log("example 6    " + num6);
+num6 = num6 +1;
+};
+//exercice 7 
+let num7 = 81;
+while(num7>=26){
+    console.log("example7   "+ num7);
+num7 = num7 -1;
+   
+};
+
+// exercice 8 
+
+for (let i= 1; i < 91; i++)
+{
+if( i % 3 === 0){
+    console.log("test 8    "+ i);
+
+}
+}
+//exercice 5
+for( let i= 98; i>= 0; i--)
+{
+    if (i %2 === 1 ){
+        console.log("the odd nb is    " +i);
+    }
+}
